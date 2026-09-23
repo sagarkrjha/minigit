48227ff2e45851258017df1f4ec28a8d19dc2ac9
devsagarkrjha <devsagarkumarjha@gmail.com>
2026-09-23 19:15:23 +0530

    chore(release): bump version to 1.11.1

--- src/core/version.h
+++ src/core/version.h
@@ -4,9 +4,9 @@
 
 namespace minigit::core {
 
-inline constexpr std::string_view MINIGIT_VERSION = "1.11.0";
+inline constexpr std::string_view MINIGIT_VERSION = "1.11.1";
 inline constexpr int MINIGIT_VERSION_MAJOR = 1;
 inline constexpr int MINIGIT_VERSION_MINOR = 11;
-inline constexpr int MINIGIT_VERSION_PATCH = 0;
+inline constexpr int MINIGIT_VERSION_PATCH = 1;
 
 } // namespace minigit::core
